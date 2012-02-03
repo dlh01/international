7edc4a366f6969e577794087e4f731ae32981a7d
David Herrera <herrera.dl@gmail.com>
2012-02-03 11:50:19 -0800

    update plugins

--- assets/plugins/wordpress-reset/readme.txt
+++ assets/plugins/wordpress-reset/readme.txt
@@ -1,24 +1,22 @@
 === WordPress Reset ===
-Contributors: sivel, jdingman
+Contributors: sivel
 Donate Link: http://sivel.net/donate
 Tags: wordpress-reset, wordpress, reset, admin
-Requires at least: 2.7
-Tested up to: 3.0
-Stable tag: 1.2
+Requires at least: 2.8
+Tested up to: 3.3
+Stable tag: 1.3.1
 
-Resets the WordPress database back to it's defaults.  Deletes all customizations and content.  Does not modify files only resets the database.
+Resets the WordPress database back to it's defaults. Deletes all customizations and content. Does not modify files only resets the database.
 
 == Description ==
 
-Resets the WordPress database back to it's defaults.  Deletes all customizations and content.  Does not modify files only resets the database.
+Resets the WordPress database back to it's defaults. Deletes all customizations and content. Does not modify files only resets the database.
 
 This plugin is very helpful for plugin and theme developers.
 
-If the admin user exists and has level_10 permissions it will be recreated with its current password and email address.  If the admin user does not exist or is a dummy account without admin permissions the username that is logged in will be recreated with its email address and current password.  The blog name is also kept.
+If the admin user exists and has level_10 permissions it will be recreated with its current password and email address. If the admin user does not exist or is a dummy account without admin permissions the username that is logged in will be recreated with its email address and current password. The blog name is also kept.
 
-The plugin will add an entry to the favorites drop down and also reactivate itself after the reset.  There is however a configuration option to keep it from reactivating after the reset.
-
-Props to [Jonathan Dingman](http://www.ginside.com/) for the idea behind this plugin, testing and feedback.
+The plugin will add an entry to the Admin Bar under the site title and has the ability to reactivate itself and other plugins after the reset.
 
 == Installation ==
 
@@ -27,9 +25,20 @@ Props to [Jonathan Dingman](http://www.ginside.com/) for the idea behind this pl
 
 == Frequently Asked Questions ==
 
-= How can I keep the plugin from automatically reactivating after the reset? =
+= How can I get this plugin to automatically reactive after the reset? =
 
-Open the wordpress-reset.php file in an editor and modify the line that reads `var $auto_reactivate = true;` to be `var $auto_reactivate = false;`
+Add `define( 'REACTIVATE_WP_RESET', true );` to `wp-config.php` above `/* That's all, stop editing! Happy blogging. */`
+
+= Can this plugin reactivate other plugins automatically after performing the reset? =
+
+Add an array called `$reactivate_wp_reset_additional` to the global scope by placing it in `wp-config.php` above `/* That's all, stop editing! Happy blogging. */` that contains the plugin basenames of the plugins to activate, such as:
+
+`
+$reactivate_wp_reset_additional = array(
+	'hello.php',
+	'akismet/akismet.php'
+);
+`
 
 == Upgrade ==
 
@@ -39,16 +48,28 @@ Open the wordpress-reset.php file in an editor and modify the line that reads `v
 
 == Usage ==
 
-1. Visit the WordPress Reset Tools page by either clicking the link in the favorites dropdown menu or Tools>WordPress Reset
+1. Visit the WordPress Reset Tools page by either clicking the link in the Admin Bar or Tools>WordPress Reset
 1. Type 'reset' in the text field and click reset.
 
 == Upgrade Notice ==
 
-This release fixes deprecated notices for WordPress 3.0, correctly disables the password nag and removes the randomly generate password from the new blog email.
+= 1.3.1 =
+
+Fixes a deprecated notice in WordPress 3.3, removed the $auto_reactivate variable, and look for REACTIVATE_WP_RESET to be defined in wp-config.php, as well as the ability to activate additional plugins using a global $reactivate_wp_reset_additional array defined in wp-config.php
 
 == Changelog ==
 
-= 1.2 (2010-04-04: =
+= 1.3.1 (2012-01-29): =
+* Apparently in a LIKE query, an underscore matches a single character, so we must escape it
+
+= 1.3 (2012-01-29): =
+* Fixes a deprecated notice in WordPress 3.3 when using get_userdatabylogin
+* Removes the $auto_reactivate variable
+* Look for REACTIVATE_WP_RESET constant to be defined in wp-config.php for auto reactivation after reset
+* Look for global $reactivate_wp_reset_additional for additional plugins to auto reactivate after reset
+* Add a Reset link to the admin bar under the site title
+
+= 1.2 (2010-04-04): =
 * Updates to fix deprecated notices for WP 3.0
 * Updates for 3.0 to disable password nag
 * Modify new blog email to not include the generated password
--- assets/plugins/wordpress-reset/wordpress-reset.php
+++ assets/plugins/wordpress-reset/wordpress-reset.php
@@ -2,116 +2,147 @@
 /*
 Plugin Name: WordPress Reset
 Plugin URI: http://sivel.net/wordpress/wordpress-reset/
-Description: Resets the WordPress database back to it's defaults.  Deletes all customizations and content.	Does not modify files only resets the database.
+Description: Resets the WordPress database back to it's defaults. Deletes all customizations and content. Does not modify files only resets the database.
 Author: Matt Martz
-Version: 1.2
+Version: 1.3.1
 Author URI: http://sivel.net/
 
-		Copyright (c) 2009-2010 Matt Martz (http://sivel.net)
-		WordPress Reset is released under the GNU General Public License (GPL)
-		http://www.gnu.org/licenses/gpl-2.0.txt
+	Copyright (c) 2009-2012 Matt Martz (http://sivel.net)
+	WordPress Reset is released under the GNU General Public License (GPL)
+	http://www.gnu.org/licenses/gpl-2.0.txt
 */
 
 // Only run the code if we are in the admin
 if ( is_admin() ) :
 
 class WordPressReset {
-	// Set to true by default, this allows the plugin to be automatically reactivated after the reset
-	var $auto_reactivate = true;
-
 	// Action/Filter Hooks
 	function WordPressReset() {
-		add_action('admin_menu', array(&$this, 'add_page'));
-		add_action('admin_footer', array(&$this, 'footer'));
-		add_action('admin_head', array(&$this, 'head'));
-		add_action('init', array(&$this, 'init'));
-		add_filter('favorite_actions', array(&$this, 'favorites'), 100);
-		add_filter('wp_mail', array(&$this, 'hijack_mail'), 1);
+		add_action( 'admin_menu', array( &$this, 'add_page' ) );
+		add_action( 'admin_init', array( &$this, 'admin_init' ) );
+		add_filter( 'favorite_actions', array( &$this, 'favorites' ), 100 );
+		add_action( 'wp_before_admin_bar_render', array( &$this, 'admin_bar_link' ) );
+		add_filter( 'wp_mail', array( &$this, 'hijack_mail' ), 1 );
 	}
 
 	// favorite_actions filter hook operations
 	// While this plugin is active put a link to the reset page in the favorites drop down.
-	function favorites($actions) {
-		$reset['tools.php?page=wordpress-reset'] = array('WordPress Reset', 'level_10');
-		return array_merge($reset, $actions);
+	function favorites( $actions ) {
+		$reset['tools.php?page=wordpress-reset'] = array( 'WordPress Reset', 'level_10' );
+		return array_merge( $reset, $actions );
 	}
 
-	// init action hook operations
+	// wp_before_admin_bar_render action hook operations
+	// While this plugin is active put a link to the reset page in the admin bar under the site title
+	function admin_bar_link() {
+		global $wp_admin_bar;
+		$wp_admin_bar->add_menu(
+			array(
+				'parent' => 'site-name',
+				'id'     => 'wordpress-reset',
+				'title'  => 'Reset Site',
+				'href'   => admin_url( 'tools.php?page=wordpress-reset' )
+			)
+		);
+	}
+
+	// admin_init action hook operations
 	// Checks for wordpress_reset post value and if there deletes all wp tables
 	// and performs an install, populating the users previous password also
-	function init() {
+	function admin_init() {
 		global $current_user;
-		if ( isset($_POST['wordpress_reset']) && $_POST['wordpress_reset'] == 'true' && isset($_POST['wordpress_reset_confirm']) && $_POST['wordpress_reset_confirm'] == 'reset' ) {
-			require_once(ABSPATH . '/wp-admin/includes/upgrade.php');
 
-			$blogname = get_option('blogname');
-			$admin_email = get_option('admin_email');
-			$blog_public = get_option('blog_public');
+		$wordpress_reset = ( isset( $_POST['wordpress_reset'] ) && $_POST['wordpress_reset'] == 'true' ) ? true : false;
+		$wordpress_reset_confirm = ( isset( $_POST['wordpress_reset_confirm'] ) && $_POST['wordpress_reset_confirm'] == 'reset' ) ? true : false;
+		$valid_nonce = ( isset( $_POST['_wpnonce'] ) && wp_verify_nonce( $_POST['_wpnonce'], 'wordpress_reset' ) ) ? true : false;
+
+		if ( $wordpress_reset && $wordpress_reset_confirm && $valid_nonce ) {
+			require_once( ABSPATH . '/wp-admin/includes/upgrade.php' );
+
+			$blogname = get_option( 'blogname' );
+			$admin_email = get_option( 'admin_email' );
+			$blog_public = get_option( 'blog_public' );
 
 			if ( $current_user->user_login != 'admin' )
-				$user = get_userdatabylogin('admin');
-			if ( ! isset($user) || $user->user_level < 10 )
+				$user = get_user_by( 'login', 'admin' );
+
+			if ( ! isset( $user ) || $user->user_level < 10 )
 				$user = $current_user;
 
-			global $wpdb;
-			$tables = $wpdb->get_col("SHOW TABLES LIKE '{$wpdb->prefix}%'");
-			foreach ($tables as $table) {
-				$wpdb->query("DROP TABLE $table");
+			global $wpdb, $reactivate_wp_reset_additional;
+
+			$prefix = str_replace( '_', '\_', $wpdb->prefix );
+			$tables = $wpdb->get_col( "SHOW TABLES LIKE '{$prefix}%'" );
+			foreach ( $tables as $table ) {
+				$wpdb->query( "DROP TABLE $table" );
 			}
 
-			$result = wp_install($blogname, $user->user_login, $user->user_email, $blog_public);
-			extract($result, EXTR_SKIP);
+			$result = wp_install( $blogname, $user->user_login, $user->user_email, $blog_public );
+			extract( $result, EXTR_SKIP );
 
-			$query = $wpdb->prepare("UPDATE $wpdb->users SET user_pass = %s, user_activation_key = '' WHERE ID = %d", $user->user_pass, $user_id);
-			$wpdb->query($query);
+			$query = $wpdb->prepare( "UPDATE $wpdb->users SET user_pass = %s, user_activation_key = '' WHERE ID = %d", $user->user_pass, $user_id );
+			$wpdb->query( $query );
 
-			$get_user_meta = function_exists('get_user_meta') ? 'get_user_meta' : 'get_usermeta';
-			$update_user_meta = function_exists('update_user_meta') ? 'update_user_meta' : 'update_usermeta';
+			$get_user_meta = function_exists( 'get_user_meta' ) ? 'get_user_meta' : 'get_usermeta';
+			$update_user_meta = function_exists( 'update_user_meta' ) ? 'update_user_meta' : 'update_usermeta';
 
-			if ( $get_user_meta($user_id, 'default_password_nag') )
-				$update_user_meta($user_id, 'default_password_nag', false);
-			if ( $get_user_meta($user_id, $wpdb->prefix . 'default_password_nag') )
-				$update_user_meta($user_id, $wpdb->prefix . 'default_password_nag', false);
+			if ( $get_user_meta( $user_id, 'default_password_nag' ) )
+				$update_user_meta( $user_id, 'default_password_nag', false );
 
-			if ( $this->auto_reactivate == true )
-				update_option('active_plugins', array(plugin_basename(__FILE__)));
+			if ( $get_user_meta( $user_id, $wpdb->prefix . 'default_password_nag' ) )
+				$update_user_meta( $user_id, $wpdb->prefix . 'default_password_nag', false );
+
+			if ( defined( 'REACTIVATE_WP_RESET' ) && REACTIVATE_WP_RESET === true )
+				@activate_plugin( plugin_basename( __FILE__ ) );
+
+			if ( ! empty( $reactivate_wp_reset_additional ) ) {
+				foreach ( $reactivate_wp_reset_additional as $plugin ) {
+					$plugin = plugin_basename( $plugin );
+					if ( ! is_wp_error( validate_plugin( $plugin ) ) )
+						@activate_plugin( $plugin );
+				}
+			}
 
 			wp_clear_auth_cookie();
-			wp_set_auth_cookie($user_id);
+			wp_set_auth_cookie( $user_id );
 
-			wp_redirect(admin_url() . '?reset');
+			wp_redirect( admin_url() . '?reset' );
 			exit();
 		}
 
-		if ( array_key_exists('reset', $_GET) && stristr($_SERVER['HTTP_REFERER'], 'wordpress-reset') ) {
-			$user = get_userdata(1);
-			add_action('admin_notices',create_function('$a', 'echo \'<div id="message" class="updated fade"><p><strong>WordPress has been reset back to defaults.  The user "' . $user->user_login . '" was recreated with its previous password.</strong></p></div>\';'));
-			do_action('wordpress_reset_post', $user);
+		if ( array_key_exists( 'reset', $_GET ) && stristr( $_SERVER['HTTP_REFERER'], 'wordpress-reset' ) ) {
+			add_action( 'admin_notices', function () {
+				$user = get_user_by( 'id', 1 );
+				echo '<div id="message" class="updated fade"><p><strong>WordPress has been reset back to defaults. The user "' . $user->user_login . '" was recreated with its previous password.</strong></p></div>';
+				do_action( 'wordpress_reset_post', $user );
+			} );
 		}
 	}
 
 	// Overwrite the password, because we actually reset it after this email goes out
-	function hijack_mail($args) {
-		if ( stristr($args['message'], 'Your new WordPress blog has been successfully set up at') )
-			$args['message'] = preg_replace('/Password:.+/', 'Password: Previously specified password', $args['message']);
+	function hijack_mail( $args ) {
+		if ( preg_match( '/Your new WordPress (blog|site) has been successfully set up at/i', $args['message'] ) ) {
+			$args['message'] = str_replace( 'Your new WordPress site has been successfully set up at:', 'Your WordPress site has been successfully reset, and can be accessed at:', $args['message'] );
+			$args['message'] = preg_replace( '/Password:.+/', 'Password: previously specified password', $args['message'] );
+		}
 		return $args;
 	}
 
-	// admin_head action hook operations
+	// admin_print_scripts action hook operations
 	// Enqueue jQuery to the head
-	function head() {
-		wp_enqueue_script('jquery');
+	function admin_js() {
+		wp_enqueue_script( 'jquery' );
 	}
 
 	// admin_footer action hook operations
 	// Do some jQuery stuff to warn the user before submission
-	function footer() {
+	function footer_js() {
 	?>
 	<script type="text/javascript">
 	/* <![CDATA[ */
 		jQuery('#wordpress_reset_submit').click(function(){
 			if ( jQuery('#wordpress_reset_confirm').val() == 'reset' ) {
-				var message = 'This action is not reversable.\n\nClicking "OK" will reset your database back to it\'s defaults.  Click "Cancel" to abort.'
+				var message = 'This action is not reversable.\n\nClicking "OK" will reset your database back to it\'s defaults. Click "Cancel" to abort.'
 				var reset = confirm(message);
 				if ( reset ) {
 					jQuery('#wordpress_reset_form').submit();
@@ -120,7 +151,7 @@ class WordPressReset {
 					return false;
 				}
 			} else {
-				alert('Invalid confirmation word.  Please type the word \'reset\' in the confirmation field.');
+				alert('Invalid confirmation word. Please type the word \'reset\' in the confirmation field.');
 				return false;
 			}
 		});
@@ -132,38 +163,72 @@ class WordPressReset {
 	// admin_menu action hook operations
 	// Add the settings page
 	function add_page() {
-		if ( current_user_can('level_10') && function_exists('add_management_page') ) :
-			add_management_page('WordPress Reset', 'WordPress Reset', 'level_10', 'wordpress-reset', array(&$this, 'admin_page'));
-		endif;
+		if ( current_user_can( 'level_10' ) && function_exists( 'add_management_page' ) )
+			$hook = add_management_page( 'Reset', 'Reset', 'level_10', 'wordpress-reset', array( &$this, 'admin_page' ) );
+			add_action( "admin_print_scripts-{$hook}", array( &$this, 'admin_js' ) );
+			add_action( "admin_footer-{$hook}", array( &$this, 'footer_js' ) );
 	}
 
 	// add_option_page callback operations
 	// The settings page
 	function admin_page() {
-		global $current_user;
-		if ( isset($_POST['wordpress_reset_confirm']) && $_POST['wordpress_reset_confirm'] != 'reset' )
-			echo '<div id="message" class="error fade"><p><strong>Invalid confirmation word.  Please type the word \'reset\' in the confirmation field.</strong></p></div>';
+		global $current_user, $reactivate_wp_reset_additional;
+		if ( isset( $_POST['wordpress_reset_confirm'] ) && $_POST['wordpress_reset_confirm'] != 'reset' )
+			echo '<div class="error fade"><p><strong>Invalid confirmation word. Please type the word \'reset\' in the confirmation field.</strong></p></div>';
+		elseif ( isset( $_POST['_wpnonce'] ) )
+			echo '<div class="error fade"><p><strong>Invalid nonce. Please try again.</strong></p></div>';
+
+		$missing = array();
+		if ( ! empty( $reactivate_wp_reset_additional ) ) {
+			foreach ( $reactivate_wp_reset_additional as $key => $plugin ) {
+				if ( is_wp_error( validate_plugin( $plugin ) ) ) {
+					unset( $reactivate_wp_reset_additional[$key] );
+					$missing[] = $plugin;
+				}
+			}
+		}
+
+		$will_reactivate = ( defined( 'REACTIVATE_WP_RESET') && REACTIVATE_WP_RESET === true ) ? true : false;
 	?>
 	<div class="wrap">
-		<h2>WordPress Reset</h2>
-		<br />
-		<form id="wordpress_reset_form" action="" method="post">
-			<input id="wordpress_reset" type="hidden" name="wordpress_reset" value="true" />
-			Type 'reset' in the confirmation field to confirm the reset and then click the reset button:<br /><br />
-			<input id="wordpress_reset_confirm" type="text" name="wordpress_reset_confirm" value="" />&nbsp;&nbsp;<input id="wordpress_reset_submit" type="submit" name="Submit" class="button-primary" value="Reset" /><br /><br />
-			<ul>
-				<li><strong>After completing this reset you will taken to the dashboard.</strong></li>
-<?php
-			$admin = get_userdatabylogin('admin');
-			if ( ! isset($admin->user_login) || $admin->user_level < 10 ) :
-				$user = $current_user;
-?>
-				<li>The 'admin' user does not exist.  The user '<strong><?php echo $user->user_login; ?></strong>' will be recreated with its current password with user level 10.</li>
-			<?php else : ?>
-				<li>The '<strong>admin</strong>' user exists and will be recreated with its current password.</li>
-			<?php endif; ?>
-				<li>This plugin <strong>will<?php echo $this->auto_reactivate ? '</strong> ' : ' not</strong> ';?>be automatically reactivated after the reset.</li>
+		<div id="icon-tools" class="icon32"><br /></div>
+		<h2>Reset</h2>
+		<h3>Details about the reset</h3>
+		<p><strong>After completing this reset you will taken to the dashboard.</strong></p>
+		<?php $admin = get_user_by( 'login', 'admin' ); ?>
+		<?php if ( ! isset( $admin->user_login ) || $admin->user_level < 10 ) : $user = $current_user; ?>
+		<p>The 'admin' user does not exist. The user '<strong><?php echo esc_html( $user->user_login ); ?></strong>' will be recreated with its <strong>current password</strong> with user level 10.</p>
+		<?php else : ?>
+		<p>The '<strong>admin</strong>' user exists and will be recreated with its <strong>current password</strong>.</p>
+		<?php endif; ?>
+		<p>This plugin <strong>will<?php echo $will_reactivate ? '' : ' not';?> be automatically reactivated</strong> after the reset. <?php echo ! $will_reactivate ? "To have this plugin auto-reactivate add <span class='code'>define( 'REACTIVATE_WP_RESET', true );</span> to <span class='code'>wp-config.php</span>." : ''; ?></p>
+		<?php if ( ! empty( $reactivate_wp_reset_additional ) ) : ?>
+		<p>The following <strong>additional plugins will be reactivated</strong>:
+			<ul style="list-style-type: disc;">
+			<?php foreach ( $reactivate_wp_reset_additional as $plugin ) : $plugin_data = get_plugin_data( WP_PLUGIN_DIR . '/' . $plugin ); ?>
+				<li style="margin: 5px 0 0 30px;"><strong><?php echo esc_html( $plugin_data['Name'] ); ?></strong></li>
+			<?php endforeach; unset( $reactivate_wp_reset_additional, $plugin, $plugin_data ); ?>
 			</ul>
+		</p>
+		<?php endif; ?>
+		<?php if ( ! empty( $missing ) ) : ?>
+		<p>The following <strong>additional plugins are missing</strong> and cannot be reactivated:
+			<ul style="list-style-type: disc;">
+			<?php foreach ( $missing as $plugin ) : ?>
+				<li style="margin: 5px 0 0 30px;"><strong><?php echo esc_html( $plugin ); ?></strong></li>
+			<?php endforeach; unset( $missing, $plugin ); ?>
+			</ul>
+		</p>
+		<?php endif; ?>
+		<h3>Reset</h3>
+		<p>Type '<strong>reset</strong>' in the confirmation field to confirm the reset and then click the reset button:</p>
+		<form id="wordpress_reset_form" action="" method="post">
+			<?php wp_nonce_field( 'wordpress_reset' ); ?>
+			<input id="wordpress_reset" type="hidden" name="wordpress_reset" value="true" />
+			<input id="wordpress_reset_confirm" type="text" name="wordpress_reset_confirm" value="" />
+			<p class="submit">
+				<input id="wordpress_reset_submit" style="width: 80px;" type="submit" name="Submit" class="button-primary" value="Reset" />
+			</p>
 		</form>
 	</div>
 	<?php
@@ -175,4 +240,3 @@ $WordPressReset = new WordPressReset();
 
 // End if for is_admin
 endif;
-?>
