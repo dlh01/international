0aa03f27fd33e0a1cbb61447a300a5cf6d9964a1
David Herrera <herrera.dl@gmail.com>
2012-03-02 17:16:29 -0800

    Change location archive header to "Resources For"
    closes #39

--- assets/themes/international/taxonomy-location.php
+++ assets/themes/international/taxonomy-location.php
@@ -13,7 +13,7 @@ get_header(); ?>
 
                 <header class="page-header">
                 <h1 class="page-title"><?php
-printf( __( 'Archives: %s', 'toolbox' ), '<span>' . single_cat_title( '', false ) . '</span>' );
+printf( __( 'Resources For: %s', 'toolbox' ), '<span>' . single_cat_title( '', false ) . '</span>' );
 ?></h1>
 
 <?php
