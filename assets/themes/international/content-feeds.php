dc85c269068c088552aa78f550a31cf82d951517
David Herrera <herrera.dl@gmail.com>
2012-03-28 16:37:48 -0700

    Display an RSS feed using the core RSS widget and data from Advanced Custom Fields
    closes #70, #71, #72, #73, #74, #75

--- assets/themes/international/content-feeds.php
+++ assets/themes/international/content-feeds.php
@@ -8,14 +8,44 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<?php
+/**
+ *
+ * Display an RSS feed using the core RSS widget and data from Advanced Custom Fields
+ *
+ */
 
-  <?php the_widget('WP_Widget_RSS', array(
-    'url' => 'http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml'
-  )); ?> 
+/* Get each Repeater row from Advanced Custom fields */
+$rows = get_field('the_feeds');
 
+/* If there are rows, proceed */
+if ( $rows ) {
+  /**
+   * For each row of the Repeater field
+   */
+  foreach ( $rows as $row ) {
+    /* Store the instance arguments from the Repeater field in a variable */
+    $instance_arguments = array(
+      'title' => $row['the_feed_title'],
+      'url' => $row['the_feed_url'],
+      'items' => 5,
+      'show_author' => true,
+      'show_date' => true,
+    );
+    /* Store the widget arguments in a variable */
+    $widget_arguments = array(
+      /* Wrap the widget title in an h3, not the default h2 */
+      'before_title' => '<h3 class="widgettitle">',
+      'after_title' => '</h3>',
+    );
+    /* Wrap the widget instance in a <section> */
+    echo '<section>';
+    /* Create a new widget instance and use the variables above for instance and widget arguments */
+    the_widget( 'WP_Widget_RSS', $instance_arguments, $widget_arguments );
+    /* Close the widget <section> */
+    echo '</section>';
+  }
+}
+?> 
 
- <?php the_widget('WP_Widget_Archives'); ?> 
-
-
-</article><!-- #post-<?php the_ID(); ?> -->
+</section>
