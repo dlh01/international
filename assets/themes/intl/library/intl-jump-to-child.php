95f4e4a85156b5b36294843064651e274c0e6522
David Herrera <herrera.dl@gmail.com>
2012-05-27 21:37:52 -0700

    Create separate library file for the jump-to-child code

---
+++ assets/themes/intl/library/intl-jump-to-child.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Jump to the child Locations of the Location we're currently viewing
+ */
+
+function intl_jump_to_child() {
+  // Get all Term data from the database for the Location term we're currently viewing
+  $thislocation = get_term_by( 'name', single_cat_title( '', false ), 'location' ); 
+
+  // Show the child terms of the Term we're currently viewing using the term_id gathered above
+  wp_list_categories( array(
+    'taxonomy' => 'location',
+    'title_li' => '',
+    'child_of' => $thislocation->term_id,
+    'depth' => 1
+  ));
+}
+?>
