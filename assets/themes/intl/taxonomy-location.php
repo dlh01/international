8deb5196ba96e6edb7caeed6a3d4141d396d6258
David Herrera <herrera.dl@gmail.com>
2012-05-27 21:10:51 -0700

    Paste in the posts-by-Resource-Type loop

--- assets/themes/intl/taxonomy-location.php
+++ assets/themes/intl/taxonomy-location.php
@@ -28,6 +28,38 @@ get_header(); ?>
 
 				<?php twentyeleven_content_nav( 'nav-above' ); ?>
 
+        <?php
+        $location_query_var = get_query_var( 'location' );
+        $resource_type_terms = get_terms( 'resource-type' );
+        foreach ( $resource_type_terms as $type ) {
+          $args = array(
+            'tax_query' => array(
+              array(
+                'taxonomy'  => 'location',
+                'field'     => 'slug',
+                'terms'     => $location_query_var,
+                'include_children'  => false,
+              ),
+              array(
+                'taxonomy'  => 'resource-type',
+                'field'     => 'slug',
+                'terms'     => $type,
+              )
+            ),
+            'posts_per_page' => 3,
+          );
+          $the_query = new WP_Query( $args );
+          if ( $the_query->have_posts() ) :
+            echo '<section>'; // group each list of resources in a <section>
+              echo '<h1>' . $type->name . '</h1>';
+                while ( $the_query->have_posts() ) : $the_query->the_post();
+                  get_template_part( 'content', 'post' );
+                endwhile;
+            echo '</section>';
+          endif;
+        }
+        wp_reset_postdata();
+        ?>
 
 				<?php twentyeleven_content_nav( 'nav-below' ); ?>
 
