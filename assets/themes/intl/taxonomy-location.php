a277c0f1cc683205f1c2418193135ed72fc87ed4
David Herrera <herrera.dl@gmail.com>
2012-05-27 21:03:06 -0700

    Remove default loop for displaying posts

--- assets/themes/intl/taxonomy-location.php
+++ assets/themes/intl/taxonomy-location.php
@@ -28,18 +28,6 @@ get_header(); ?>
 
 				<?php twentyeleven_content_nav( 'nav-above' ); ?>
 
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
-
-					<?php
-						/* Include the Post-Format-specific template for the content.
-						 * If you want to overload this in a child theme then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'content', get_post_format() );
-					?>
-
-				<?php endwhile; ?>
 
 				<?php twentyeleven_content_nav( 'nav-below' ); ?>
 
