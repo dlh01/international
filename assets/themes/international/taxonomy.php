32531f3cfb27b6c0b3686b81c41d578ab31d6b61
David Herrera <herrera.dl@gmail.com>
2012-05-28 11:12:37 -0700

    Determine how to display the current taxonomy archive using the query string

---
+++ assets/themes/international/taxonomy.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * The template for displaying Taxonomy Archive pages.
+ */
+
+get_header(); ?>
+
+		<section id="primary">
+			<div id="content" role="main">
+
+			<?php if ( have_posts() ) : ?>
+
+				<header class="page-header">
+					<h1 class="page-title"><?php
+						printf( __( 'Taxonomy Archives: %s', 'twentyeleven' ), '<span>' . single_cat_title( '', false ) . '</span>' );
+					?></h1>
+
+					<?php
+						$category_description = category_description();
+						if ( ! empty( $category_description ) )
+							echo apply_filters( 'category_archive_meta', '<div class="category-archive-meta">' . $category_description . '</div>' );
+					?>
+				</header>
+
+				<?php twentyeleven_content_nav( 'nav-above' ); ?>
+
+        <?php /* Determine how to display the current taxonomy archive using the query string */ ?>
+        <?php /* If the query is looking for both a resource type and a location, then use the normal loop */ ?>
+        <?php if ( get_query_var( 'location' ) && get_query_var( 'resource-type' ) ) : ?>
+        <?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+					<?php get_template_part( 'archive', 'location' ); ?> 
+				<?php endwhile; ?>
+        <?php /* If the query is looking for just a location, then hijack the loop and run the separate, multiple instances of WP_Query */ ?>
+        <?php elseif ( get_query_var( 'location' ) ) : ?>
+          <?php intl_list_resources_in_location_by_type(); ?>
+        <?php /* Otherwise, run the loop normally and use the archive-location.php view. This would need to be changed if there are other types of posts that could display */ ?>
+        <?php else: ?>
+          <?php while ( have_posts() ) : the_post(); ?>
+            <?php get_template_part( 'archive', 'location' ); ?> 
+          <?php endwhile; ?>
+        <?php endif; ?>
+
+				<?php twentyeleven_content_nav( 'nav-below' ); ?>
+
+			<?php else : ?>
+
+				<article id="post-0" class="post no-results not-found">
+					<header class="entry-header">
+						<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
+					</header><!-- .entry-header -->
+
+					<div class="entry-content">
+						<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyeleven' ); ?></p>
+						<?php get_search_form(); ?>
+					</div><!-- .entry-content -->
+				</article><!-- #post-0 -->
+
+			<?php endif; ?>
+
+			</div><!-- #content -->
+		</section><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
