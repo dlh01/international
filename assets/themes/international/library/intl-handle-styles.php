6cc989165c8b212c453003580595df5c97283085
David Herrera <herrera.dl@gmail.com>
2012-05-30 17:18:51 -0700

    Add library file for handling stylesheets

---
+++ assets/themes/international/library/intl-handle-styles.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *
+ * Register and enqueue stylesheets
+ *
+ */
+add_action( 'wp_enqueue_scripts', 'intl_handle_styles' );
+function intl_handle_styles() {
+
+  // Register stylesheet
+  wp_register_style(
+    'international', // handle
+    get_stylesheet_directory_uri() . 'international.css', // src
+    '', // deps
+    '0.1' // version
+  );
+
+  /**
+   * Enqueue needed styles
+   */
+
+  wp_enqueue_style( 'international' );
+
+}
+?>
