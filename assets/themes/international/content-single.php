1f9913528cffc4577d91474253149dbbcdd6aabd
David Herrera <herrera.dl@gmail.com>
2012-06-06 00:19:31 -0700

    Show the Location, RT, modified date, and contact link in .entry-meta

--- assets/themes/international/content-single.php
+++ assets/themes/international/content-single.php
@@ -53,11 +53,137 @@ $url_3_language   = get_field( 'url_3_language' );
         echo '<p class=original><a href="' . $original . '" target="_blank">View full resource</a></p>';
       endif;
     ?>
+    <table class="table table-condensed">
+    <tbody>
+  <?php
+    if ( get_field( 'author' ) ) {
+      echo '<tr>';
+      echo '<td class=field>Author</td class=field>';
+      echo '<td>' . $author . '</td>';
+      echo '</tr>';
+    }
+    if ( get_field( 'publication' ) ) {
+      echo '<tr>';
+      echo '<td class=field>Publication</td class=field>';
+      echo '<td>' . $publication . '</td>';
+      echo '</tr>';
+    }
+    if ( get_field( 'organization' ) ) {
+      echo '<tr>';
+      echo '<td class=field>Organization</td class=field>';
+      echo '<td>' . $organization . '</td>';
+      echo '</tr>';
+    }
+    if ( get_field( 'date' ) ) {
+      echo '<tr>';
+      echo '<td class=field>Date</td class=field>';
+      echo '<td>' . $date . '</td>';
+      echo '</tr>';
+    }
+    if ( get_field( 'year' ) ) {
+      echo '<tr>';
+      echo '<td class=field>Year</td class=field>';
+      echo '<td>' . $year . '</td>';
+      echo '</tr>';
+    }
+    if ( get_field( 'contact' ) ) {
+      echo '<tr>';
+      echo '<td class=field>Contact Address</td class=field>';
+      echo '<td>' . $contact . '</td>';
+      echo '</tr>';
+    }
+    if ( get_field( 'twitter' ) ) {
+      echo '<tr>';
+      echo '<td class=field>Twitter</td class=field>';
+      echo '<td><a href="http://www.twitter.com/' . $twitter_handle . '">@' . $twitter_handle . '</a></td>';
+      echo '</tr>';
+    }
+    if ( get_field( 'facebook' ) ) {
+      echo '<tr>';
+      echo '<td class=field>Facebook</td class=field>';
+      echo '<td><a href="' . $facebook_page . '">' . $facebook_page . '</a></td>';
+      echo '</tr>';
+    }
+    if ( get_field( 'socmed' ) ) {
+      echo '<tr>';
+      echo '<td class=field>Other Social Media</td class=field>';
+      echo '<td>' . $socmed . '</td>';
+      echo '</tr>';
+    }
+    if ( get_field( 'volume' ) ) {
+      echo '<tr>';
+      echo '<td class=field>Volume</td class=field>';
+      echo '<td>' . $volume . '</td>';
+      echo '</tr>';
+    }
+    if ( get_field( 'issue' ) ) {
+      echo '<tr>';
+      echo '<td class=field>Issue</td class=field>';
+      echo '<td>' . $issue . '</td>';
+      echo '</tr>';
+    }
+    if ( get_field( 'page' ) ) {
+      echo '<tr>';
+      echo '<td class=field>Pages</td class=field>';
+      echo '<td>' . $page . '</td>';
+      echo '</tr>';
+    }
+    if ( get_field( 'deadline' ) ) {
+      echo '<tr>';
+      echo '<td class=field>Application Deadline</td class=field>';
+      echo '<td>' . $deadline . '</td>';
+      echo '</tr>';
+    }
+    if ( get_field( 'cost' ) ) {
+      echo '<tr>';
+      echo '<td class=field>Cost</td class=field>';
+      echo '<td>' . $cost . '</td>';
+      echo '</tr>';
+    }
+    if ( get_field( 'language' ) ) {
+      echo '<tr>';
+      echo '<td class=field>Language</td class=field>';
+      echo '<td>' . $language . '</td>';
+      echo '</tr>';
+    }
+    if ( get_field( 'url_2' ) ) {
+      echo '<tr>';
+      echo '<td class=field>URL 2</td class=field>';
+      echo '<td><a href="' . $url_2 . '">' . $url_2 . '</a></td>';
+      echo '</tr>';
+    }
+    if ( get_field( 'url_2_language' ) ) {
+      echo '<tr>';
+      echo '<td class=field>URL 2 language</td class=field>';
+      echo '<td>' . $url_2_language . '</td>';
+      echo '</tr>';
+    }
+    if ( get_field( 'url_3' ) ) {
+      echo '<tr>';
+      echo '<td class=field>URL 3</td class=field>';
+      echo '<td><a href="' . $url_3 . '">' . $url_3 . '</a></td>';
+      echo '</tr>';
+    }
+    if ( get_field( 'url_3_language' ) ) {
+      echo '<tr>';
+      echo '<td class=field>URL 3 language</td class=field>';
+      echo '<td>' . $url_3_language . '</td>';
+      echo '</tr>';
+    }
+  ?>
+    </tbody>
+    </table>
+
 		<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
 	</div><!-- .entry-content -->
 
 	<footer class="entry-meta">
 		<?php
+        _e( get_the_term_list( $post->ID, 'location', 'Locations: ', ', ', '. ' ) );
+        _e( get_the_term_list( $post->ID, 'resource-type', 'Resource Type: ', ', ', '. ' ) );
+        _e("Updated on ", "toolbox"); the_modified_date(get_option('date_format')); _e(". ");
+        _e("<a href='/contact'>Suggest an update to this entry</a>.");
+
 			/* translators: used between list items, there is a space after the comma */
 			$categories_list = get_the_category_list( __( ', ', 'twentyeleven' ) );
 
