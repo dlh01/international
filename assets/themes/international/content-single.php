ce6c5506c86c31d62592a799b2822237904fdbef
David Herrera <herrera.dl@gmail.com>
2012-06-06 00:24:45 -0700

    Use just the .entry-meta from the legacy theme

--- assets/themes/international/content-single.php
+++ assets/themes/international/content-single.php
@@ -179,33 +179,11 @@ $url_3_language   = get_field( 'url_3_language' );
 
 	<footer class="entry-meta">
 		<?php
-        _e( get_the_term_list( $post->ID, 'location', 'Locations: ', ', ', '. ' ) );
-        _e( get_the_term_list( $post->ID, 'resource-type', 'Resource Type: ', ', ', '. ' ) );
-        _e("Updated on ", "toolbox"); the_modified_date(get_option('date_format')); _e(". ");
-        _e("<a href='/contact'>Suggest an update to this entry</a>.");
-
-			/* translators: used between list items, there is a space after the comma */
-			$categories_list = get_the_category_list( __( ', ', 'twentyeleven' ) );
-
-			/* translators: used between list items, there is a space after the comma */
-			$tag_list = get_the_tag_list( '', __( ', ', 'twentyeleven' ) );
-			if ( '' != $tag_list ) {
-				$utility_text = __( 'This entry was posted in %1$s and tagged %2$s by <a href="%6$s">%5$s</a>. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'twentyeleven' );
-			} elseif ( '' != $categories_list ) {
-				$utility_text = __( 'This entry was posted in %1$s by <a href="%6$s">%5$s</a>. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'twentyeleven' );
-			} else {
-				$utility_text = __( 'This entry was posted by <a href="%6$s">%5$s</a>. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'twentyeleven' );
-			}
-
-			printf(
-				$utility_text,
-				$categories_list,
-				$tag_list,
-				esc_url( get_permalink() ),
-				the_title_attribute( 'echo=0' ),
-				get_the_author(),
-				esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) )
-			);
+      _e( get_the_term_list( $post->ID, 'location', 'Locations: ', ', ', '. ' ) );
+      _e( get_the_term_list( $post->ID, 'resource-type', 'Resource Type: ', ', ', '. ' ) );
+      twentyeleven_posted_on(); _e(". ");
+      _e("Updated on ", "toolbox"); the_modified_date(get_option('date_format')); _e(". ");
+      _e("<a href='/contact'>Suggest an update to this entry</a>. ");
 		?>
 		<?php edit_post_link( __( 'Edit', 'twentyeleven' ), '<span class="edit-link">', '</span>' ); ?>
 
