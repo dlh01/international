d5d2b2bc24af0ead8baf6b955673bde76463c099
David Herrera <herrera.dl@gmail.com>
2012-06-09 13:38:25 -0700

    Add per-section classes and clean up code

--- assets/themes/international/library/intl-list-resources-in-location-by-type.php
+++ assets/themes/international/library/intl-list-resources-in-location-by-type.php
@@ -26,14 +26,14 @@ function intl_list_resources_in_location_by_type() {
     );
     $the_query = new WP_Query( $args );
     if ( $the_query->have_posts() ) :
-      echo '<section>'; // group each list of resources in a <section>
-    echo '<h1>' . $type->name . '</h1>';
-    while ( $the_query->have_posts() ) : $the_query->the_post();
-    get_template_part( 'archive', 'location' );
-endwhile;
-echo '<footer class="entry-meta"><a href="' . esc_url( home_url( '/?resource-type=' . $type->slug . '&location=' . $location_query_var  ) ) . '"</a>View all resources about ' . single_cat_title( '', false ) .' under ' . $type->name . ' &rarr;</a></footer>';
-echo '</section>';
-endif;
+      echo '<section class="location-by-type ' . $type->slug . '">'; // group each list of resources in a <section>
+        echo '<h1 class="location-by-type--heading">' . $type->name . '</h1>';
+        while ( $the_query->have_posts() ) : $the_query->the_post();
+          get_template_part( 'archive', 'location' );
+        endwhile;
+        echo '<footer class="entry-meta location-by-type--footer"><a href="' . esc_url( home_url( '/?resource-type=' . $type->slug . '&location=' . $location_query_var  ) ) . '"</a>View all resources about ' . single_cat_title( '', false ) .' under ' . $type->name . ' &rarr;</a></footer>';
+      echo '</section>';
+    endif;
   }
   wp_reset_postdata();
 }
