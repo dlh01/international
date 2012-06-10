eb4c2c24c59ad5c1188dc9c1e52da355a5173e64
David Herrera <herrera.dl@gmail.com>
2012-06-09 17:26:15 -0700

    Language tweaks

--- assets/themes/international/taxonomy.php
+++ assets/themes/international/taxonomy.php
@@ -12,7 +12,7 @@ get_header(); ?>
 
 				<header class="page-header">
 					<h1 class="page-title"><?php
-						printf( __( 'Taxonomy Archives: %s', 'twentyeleven' ), '<span>' . single_cat_title( '', false ) . '</span>' );
+						printf( __( 'Archives: %s', 'twentyeleven' ), '<span>' . single_cat_title( '', false ) . '</span>' );
 					?></h1>
 
 					<?php
