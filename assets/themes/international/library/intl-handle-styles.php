bd0e94b4b6a20843040eccc753394edd2af5d16f
David Herrera <herrera.dl@gmail.com>
2012-06-16 20:06:44 -0700

    Bump version to 1.6.0

--- assets/themes/international/library/intl-handle-styles.php
+++ assets/themes/international/library/intl-handle-styles.php
@@ -12,7 +12,7 @@ function intl_handle_styles() {
     'international', // handle
     get_stylesheet_directory_uri() . '/css/international.css', // src
     '', // deps
-    '0.1' // version
+    '1.6.0' // version
   );
 
   // Register FontAwesome stylesheet
