3419ac3a5af0d45a2755b1a12e81a84bb8d1aa84
David Herrera <herrera.dl@gmail.com>
2012-06-17 01:02:46 -0700

    Enqueue, don't import, twentyeleven/style.css
    CSSLint.net notes that @import disallows parallel downloads
    https://github.com/stubbornella/csslint/wiki/Disallow-%40import

--- assets/themes/international/library/intl-handle-styles.php
+++ assets/themes/international/library/intl-handle-styles.php
@@ -7,11 +7,17 @@
 add_action( 'wp_enqueue_scripts', 'intl_handle_styles' );
 function intl_handle_styles() {
 
+  // Register twentyeleven stylesheet
+  wp_register_style(
+    'twentyeleven',
+    get_template_directory_uri() . '/style.css'
+  );
+
   // Register stylesheet
   wp_register_style(
     'international', // handle
     get_stylesheet_directory_uri() . '/css/international.css', // src
-    '', // deps
+    'twentyeleven', // deps
     '1.6.3' // version
   );
 
@@ -33,6 +39,7 @@ function intl_handle_styles() {
    * Enqueue needed styles
    */
 
+  wp_enqueue_style( 'twentyeleven' );
   wp_enqueue_style( 'international' );
   wp_enqueue_style( 'fontawesome' );
   wp_enqueue_style( 'jquery-ui' );
