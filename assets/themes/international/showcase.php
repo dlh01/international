519aa182bf1a63338900f3df6651763e2a7fe972
David Herrera <herrera.dl@gmail.com>
2012-07-11 13:29:46 -0700

    Comma-separated list of links to Resource Types on the home page

--- assets/themes/international/showcase.php
+++ assets/themes/international/showcase.php
@@ -74,6 +74,42 @@ get_header(); ?>
               </ul>
             </section>
             <section class="section lists">
+              <p>
+                <strong>Or browse by Resource Type</strong>: 
+                <?php
+                /**
+                 * Display a link to the archive page of each Resource Type term
+                 */
+
+                /* Get an array of data for each term in the Resource Type taxonomy */
+                $resourceterms = get_terms( 'resource-type' );
+                /**
+                 * Count the total number of terms returned. We use this number to ensure
+                 * that a comma isn't displayed after the last term
+                 */
+                $numberofresourceterms = count( $resourceterms );
+                /**
+                 * Declare our counter variable; start at 1, not 0, because
+                 * $numberofresourceterms starts at 1
+                 */
+                $i = 1;
+                foreach ( $resourceterms as $term ) {
+                  echo '<a href="/resource-type/' . $term->slug . '" title="' . sprintf(__('View all post filed under %s', 'twentyeleven'), $term->name) . '">' . $term->name . '</a>';
+                  /**
+                   * Display a comma after each link only if $i is not equal to the total
+                   * number of terms as counted in $numberofresourceterms. If they are equal
+                   * then we know we're at the last item in the array
+                   */
+                  if ( $numberofresourceterms != $i ) {
+                    echo ", ";
+                  }
+                  $i++;
+                }
+                ?>
+              </p>
+            </section>
+
+            <section class="section lists">
               <?php
                 /**
                  * Get arrays of data for the alphabetic and geographic page
